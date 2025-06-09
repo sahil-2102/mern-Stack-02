59ce1204302484607d4f1550fd3060915a1a079e
Sahil <sk4386840@gmail.com>
2025-06-09 15:03:48 +0530

    feat: setup MongoDB connection with mongoose and config file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 const app = express();
 app.get('/api/ping', (req,res) => {
     res.json({message: "pong"});
 });
-
+connectDB();
 app.listen(process.env.PORT,() => {
     console.log(`server running on port : ${process.env.PORT}`);
 });
