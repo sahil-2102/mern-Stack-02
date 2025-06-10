60806e55a191d08b8f8cff5b897bb981a66a11f5
Sahil <sk4386840@gmail.com>
2025-06-10 12:21:34 +0530

    updated server file with created routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import noteRoutes from "./routes/note.routes.js";
 dotenv.config();
 const app = express();
-app.get('/api/ping', (req,res) => {
-    res.json({message: "pong"});
-});
+app.use(express.json()); // Needed to parse JSON body
+app.use("/api/notes", noteRoutes);
 connectDB();
 app.listen(process.env.PORT,() => {
     console.log(`server running on port : ${process.env.PORT}`);
